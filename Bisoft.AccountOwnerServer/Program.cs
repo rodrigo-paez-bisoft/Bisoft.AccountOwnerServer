c67e4c471d1727338f5666e5b70d0d20d3f4abc3
Rodrigo Páez <rodrigo.paez@bisoft.com.mx>
2026-07-09 08:51:31 -0700

    fjdjfjd

---
+++ Bisoft.AccountOwnerServer/Program.cs
@@ -0,0 +1,46 @@
+
+using Bisoft.AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace Bisoft.AccountOwnerServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.ConfigureCors();
+            builder.Services.ConfigureIISIntegration();
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
